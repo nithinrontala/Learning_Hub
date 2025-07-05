6883f3f8ba5bb1c17e3b76d37872e393cfa1169b
nithinrontala <nithinrontala02@gmail.com>
2025-07-05 21:01:06 +0530

    navigations

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import './Hero.css';
+import { useNavigate } from 'react-router-dom';
 
 function Hero() {
+  const navigate = useNavigate();
   return (
     <section className="hero-section">
       <div className="hero-bg">
         <div className="hero-content">
           <h1>Discover Future-Ready Online <br />Programs</h1>
-          <a href="#courses" className="hero-btn">Browse Courses</a>
+          <button className="hero-btn" onClick={() => navigate('/courses')}>Browse Courses</button>
         </div>
       </div>
     </section>
