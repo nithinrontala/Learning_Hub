1286938e9527efdced1a6c83ccb72a12ef4d9777
nithinrontala <nithinrontala02@gmail.com>
2025-07-05 15:46:56 +0530

    hero section complete

---
+++ src/components/Hero.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './Hero.css';
+
+function Hero() {
+  return (
+    <section className="hero-section">
+      <div className="hero-bg">
+        <div className="hero-content">
+          <h1>Discover Future-Ready Online <br />Programs</h1>
+          <a href="#courses" className="hero-btn">Browse Courses</a>
+        </div>
+      </div>
+    </section>
+  );
+}
+
+export default Hero;
