34bf140030ef9037602a58835e8580005f09a8b0
sandeep <vangurisandeepkumaryadav@msitprogram.net>
2025-07-05 19:23:50 +0530

    added footer to track course cards.

--- src/components/CourseList.js
+++ src/components/CourseList.js
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, { useState } from 'react';
 import CourseCard from './CourseCard';
+import CoursePagination from './CoursePagination';
 import './CourseList.css';
 
 // Updated course data
@@ -55,12 +56,26 @@ const courses = [
 ];
 
 function CourseList() {
+  const [currentPage, setCurrentPage] = useState(1);
+  const coursesPerPage = 6;
+  const totalPages = Math.ceil(courses.length / coursesPerPage);
+  const startIdx = (currentPage - 1) * coursesPerPage;
+  const endIdx = startIdx + coursesPerPage;
+  const currentCourses = courses.slice(startIdx, endIdx);
+
   return (
-    <div className="course-list">
-      {courses.map(course => (
-        <CourseCard key={course.id} course={course} />
-      ))}
-    </div>
+    <>
+      <div className="course-list">
+        {currentCourses.map(course => (
+          <CourseCard key={course.id} course={course} />
+        ))}
+      </div>
+      <CoursePagination
+        currentPage={currentPage}
+        totalPages={totalPages}
+        onPageChange={setCurrentPage}
+      />
+    </>
   );
 }
 
